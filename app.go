0d558f8ad3a79268ee3021991cab5328d4f44c47
antoniussinaga-dev <antoniussinaga.dev@gmail.com>
2024-08-03 06:11:53 +0700

    tag

--- app.go
+++ app.go
@@ -1,6 +1,7 @@
 package aigiscache
 
 import (
+	"fmt"
 	"os"
 )
 
@@ -15,24 +16,34 @@ type App struct {
 }
 
 func New(config ...Config) AigisCache {
+	fmt.Println("Starting New function")
 	app := App{}
 	if len(config) > 0 {
 		app.config = config[0]
 	}
 	app.config = app.init()
 
+	fmt.Println("Hallo")
+
 	return &app
 }
 
 func (app *App) init() Config {
+	fmt.Println("Starting init function")
 	if app.config.file_path == "" {
 		app.config.file_path = "aigiscache.bin"
 	}
 	_, err := os.Stat(app.config.file_path)
 	if os.IsNotExist(err) {
+		fmt.Println("File does not exist, creating file")
 		if err := os.WriteFile(app.config.file_path, []byte{63, 65, 73, 71, 73, 83, 32, 67, 65, 67, 72, 69}, os.ModeDevice); err != nil {
 			panic(err.Error())
 		}
+		fmt.Println("File created successfully")
+	} else if err != nil {
+		fmt.Println("Error checking file:", err)
+	} else {
+		fmt.Println("File exists")
 	}
 	return app.config
 }
--- app_test.go
+++ app_test.go
@@ -1,15 +1,10 @@
 package aigiscache
 
 import (
-	"fmt"
-	"os"
 	"testing"
 )
 
 func Test(t *testing.T) {
 	// aigis-cache
 	New()
-	by, _ := os.ReadFile("aigiscache.bin")
-	fmt.Printf("%b", by)
-	fmt.Println("HAllo")
 }
